378308f490ef4640199518b9d3d9b551f724840c
Yash Mathur <yash97mathur@gmail.com>
2021-02-15 15:36:49 -0500

    bootstrap android project

---
+++ app/src/main/java/com/stonks/android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.stonks.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
