e6e06c7b756065be5cfff0913d71f531ec52222a
riyawalia <riyawalia12@gmail.com>
2021-03-08 11:20:40 -0500

    Remove references to wearOS added by mistake + PR comments

--- app/src/main/java/com/stonks/android/MainActivity.java
+++ app/src/main/java/com/stonks/android/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         // disable the back button on the homepage
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
