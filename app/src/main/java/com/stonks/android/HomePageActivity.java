3d6698fed9466af262df2f2c8e5a74712be939e0
Ken Tjie <ken.tjie@zemind.ca>
2021-03-10 12:05:25 -0500

    Removed 'Recent Transactions' list form the home page

--- app/src/main/java/com/stonks/android/HomePageActivity.java
+++ app/src/main/java/com/stonks/android/HomePageActivity.java
@@ -9,36 +9,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 public class HomePageActivity extends AppCompatActivity {
-    TabLayout tabLayout;
-    ViewPager viewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_pager);
+
 
         final HomePagerAdapter adapter = new HomePagerAdapter(this, getSupportFragmentManager(), 2);
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
     }
 }
