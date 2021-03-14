402adf761a7a1ad81329b4815c264a8fa834ab24
Ken Tjie <ken.tjie@zemind.ca>
2021-03-14 16:09:10 -0400

    Rebase commit

--- app/src/main/java/com/stonks/android/MainActivity.java
+++ app/src/main/java/com/stonks/android/MainActivity.java
@@ -1,9 +1,9 @@
 package com.stonks.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends BaseActivity {
 
