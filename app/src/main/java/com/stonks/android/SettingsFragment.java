499232d941dce5a5fc48ae14ff77dc1bcc038eb7
Ken Tjie <ken.tjie@zemind.ca>
2021-03-14 17:51:09 -0400

    Added additional screens for input fields

--- app/src/main/java/com/stonks/android/SettingsFragment.java
+++ app/src/main/java/com/stonks/android/SettingsFragment.java
@@ -7,6 +7,6 @@ import androidx.preference.PreferenceFragmentCompat;
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        //setPreferencesFromResource(R.xml.root_preferences, rootKey);
     }
 }
