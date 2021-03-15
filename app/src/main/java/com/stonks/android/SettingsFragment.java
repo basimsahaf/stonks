a81cdcc634623f7f802a040da23036506370405c
Ken Tjie <ken.tjie@zemind.ca>
2021-03-14 22:27:37 -0400

    Old testing code cleanup

--- app/src/main/java/com/stonks/android/SettingsFragment.java
+++ app/src/main/java/com/stonks/android/SettingsFragment.java
@@ -33,11 +33,6 @@ public class SettingsFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings_parent, container, false);
