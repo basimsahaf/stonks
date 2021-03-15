34a66de5de647a120f19100a3f468893b8a6e968
Ken Tjie <ken.tjie@zemind.ca>
2021-03-14 22:31:46 -0400

    Ran spotlessApply + small cleanup

--- app/src/main/java/com/stonks/android/RecentTransactionsFragment.java
+++ app/src/main/java/com/stonks/android/RecentTransactionsFragment.java
@@ -1,21 +1,17 @@
 package com.stonks.android;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.stonks.android.adapter.TransactionViewAdapter;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecentTransactionsFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link Fragment} subclass. Use the {@link RecentTransactionsFragment#newInstance}
+ * factory method to create an instance of this fragment.
  */
 public class RecentTransactionsFragment extends Fragment {
     public RecentTransactionsFragment() {}
@@ -28,12 +24,8 @@ public class RecentTransactionsFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_recent_transactions, container, false);
     }
 
@@ -42,7 +34,8 @@ public class RecentTransactionsFragment extends Fragment {
         RecyclerView transactionList;
         RecyclerView.Adapter transactionListAdapter;
 
-        RecyclerView.LayoutManager transactionListManager = new LinearLayoutManager(this.getContext());
+        RecyclerView.LayoutManager transactionListManager =
+                new LinearLayoutManager(this.getContext());
         transactionList = view.findViewById(R.id.history_list);
         transactionList.setLayoutManager(transactionListManager);
         transactionListAdapter = new TransactionViewAdapter(StockActivity.getFakeTransactions());
