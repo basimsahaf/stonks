afd110b2bb386d8963a0fd30a33fed8c090b9caf
Ken Tjie <ken.tjie@zemind.ca>
2021-03-29 16:58:08 -0400

    spotlessApply

--- app/src/main/java/com/stonks/android/SavedStocksFragment.java
+++ app/src/main/java/com/stonks/android/SavedStocksFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
