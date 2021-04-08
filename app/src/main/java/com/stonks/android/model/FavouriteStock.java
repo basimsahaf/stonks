05b17a752057420096acc55e291c46f705ce11ad
Riya Walia <riyawalia12@gmail.com>
2021-04-08 13:40:55 -0400

    Fix FavouriteTable bug + check if favourite exists (#109)

--- app/src/main/java/com/stonks/android/model/FavouriteStock.java
+++ app/src/main/java/com/stonks/android/model/FavouriteStock.java
@@ -1,18 +1,12 @@
 package com.stonks.android.model;
 
 public class FavouriteStock {
-    private final int id;
     private final String username;
     private final String symbol;
 
-    public FavouriteStock(int id, String username, String symbol) {
-        this.id = id;
+    public FavouriteStock(String username, String symbol) {
         this.username = username;
         this.symbol = symbol;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getUsername() {
